e96d99e61e09ec1eb2d6eeac37cedad9541a8305
ryan <22parkr@millburn.org>
2020-06-01 23:11:20 -0400

    Add number(ify), cnumber(ify) to numerical

--- interpreter/lang/numerical.py
+++ interpreter/lang/numerical.py
@@ -5,13 +5,61 @@ everything pure as possible.
 Source: https://en.wikipedia.org/wiki/Church_encoding#Calculation_with_Church_numerals
 """
 
+from interpreter.pure.lexical import Abstraction, Variable
 
-def church_numeral(number):
-    """Returns string expr of number in lambda calculus."""
-    assert isinstance(number, int) and number >= 0, "only natural numbers supported"
+
+def cnumber(num):
+    """Returns string expr of num in lambda calculus (cnum = Church numeral)."""
+    try:
+        assert not isinstance(num, float)
+        num = int(num)
+        assert num >= 0
+    except (AssertionError, ValueError):
+        raise ValueError(f"expected natural number, got '{num}'")
 
     header = "(λf.λx."
-    composed = "f (" * number
+    composed = "f (" * num
     application = "x" + ")" * composed.count("(") + ")"
 
-    return header + composed + application
+    return Abstraction(header + composed + application)
+
+
+def number(cnum):
+    """Returns str(number) given LambdaTerm cnum. If cnum isn't a Church numeral, returns None."""
+    try:
+        first_arg, first_body = cnum.nodes
+        second_arg, nth_body = first_body.nodes
+    except ValueError:
+        return None
+
+    num = 0
+    while nth_body.tokenizable:
+        var, nth_body = nth_body.nodes
+
+        if var.tokenizable or not var.like(first_arg):
+            return None
+        num += 1
+
+    return str(num) if nth_body.like(second_arg) else None
+
+
+def cnumberify(nor):
+    """In-place replacement all numbers in NormalOrderReducer with cnumbers."""
+    for node, paths in nor.flattened.items():
+        if node.expr.isdigit():
+            for path in paths:
+                nor.set(path, cnumber(node.expr))
+
+
+def numberify(nor):
+    """In-place replacement all cnumbers in NormalOrderReducer with numbers."""
+
+    def _numberify(nor, node, path):
+        num = number(node)
+        if num:
+            nor.set(path, Variable(num))
+        else:
+            for idx, sub_node in enumerate(node.nodes):
+                _numberify(nor, sub_node, path + [idx])
+
+    _numberify(nor, nor.tree, [])
--- tests/lang/numerical.py
+++ tests/lang/numerical.py
@@ -1,19 +1,28 @@
 import unittest
 
-from interpreter.lang import numerical
+from interpreter.lang.numerical import cnumber, number
 from interpreter.pure.lexical import Abstraction
 
 
 class NumericalTestCase(unittest.TestCase):
 
-    def test_to_church_numeral(self):
+    def test_cnumber(self):
         should_fail = [-2, 0.3, 4.0, 14.2]
         for case in should_fail:
-            self.assertRaises(AssertionError, numerical.church_numeral, number=case)
+            self.assertRaises(ValueError, cnumber, case)
 
         should_pass = {0: Abstraction("λf.λx.x"), 3: Abstraction("λf.λx.f (f (f (x)))")}
         for case, result in should_pass.items():
-            self.assertEqual(result, Abstraction(numerical.church_numeral(case)))
+            self.assertEqual(result, cnumber(case))
+
+    def test_number(self):
+        should_fail = [Abstraction("λf.λx.f f"), Abstraction("λf.λx.x f x")]
+        for case in should_fail:
+            self.assertEqual(number(case), case)
+
+        should_pass = {3: Abstraction("λf.λx.f (f (f (x)))"), 0: Abstraction("λf.λx.x")}
+        for result, case in should_pass.items():
+            self.assertEqual(str(result), number(case), case)
 
 
 if __name__ == '__main__':
